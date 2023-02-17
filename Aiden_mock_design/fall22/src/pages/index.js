d2aa2c5bbc5f623a8740c20f70f54cace0279e3a
jaegeun1393 <aiden1393@gmail.com>
2023-02-16 19:51:57 -0800

    s

--- Aiden_mock_design/fall22/src/pages/index.js
+++ Aiden_mock_design/fall22/src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+
 import logo from "../images/White_Transparent_AIID_short.png"
 import search from "../images/search.png"
 import submit from "../images/submit.png"
@@ -17,6 +18,7 @@ const IndexPage = () => {
   const [vdata, setvdata] = useState([
     { input_v: "", note: "" }
   ])
+  const [tags, setTags] = useState([]);
 
   const handleInputChange = (event, index) => {
     const updatedData = [...data];
@@ -30,6 +32,13 @@ const IndexPage = () => {
     setvdata(updatedData);
   };
 
+  const handleTag = (event) => {
+    const input = event.target.value;
+    const tagsArray = input.split('#').filter((tag) => tag.trim() !== '');
+    setTags(tagsArray);
+
+    console.log(tags);
+  }
 
   const handleRadioChange = (event, index) => {
     const updatedVData = [...vdata];
@@ -69,7 +78,7 @@ const IndexPage = () => {
             <button type="button" className="inline-flex text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 mr-[10px]">
               English
               <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
               </svg>
             </button>
           </div>
@@ -153,9 +162,9 @@ const IndexPage = () => {
                   >
                     <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                   <span className="">Spatial View</span>
@@ -190,9 +199,9 @@ const IndexPage = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                   <span className="">Entities</span>
@@ -208,9 +217,9 @@ const IndexPage = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M3 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 4a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                      clip-rule="evenodd"
+                      clipRule="evenodd"
                     ></path>
                   </svg>
                   <span className="">Taxonmies</span>
@@ -256,8 +265,6 @@ const IndexPage = () => {
           </div>
         </div>
 
-
-
         <div className="flex-1 px-2">
           {data.map((item, index) => (
             <div key={index}>
@@ -267,7 +274,7 @@ const IndexPage = () => {
               <div className="mb-6 pt-4 w-[1000px]">
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-easel3-fill" viewBox="0 0 16 16" className='mr-[2px]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-easel3-fill mr-[2px]" viewBox="0 0 16 16">
                       <path d="M8.5 12v1.134a1 1 0 1 1-1 0V12h-5A1.5 1.5 0 0 1 1 10.5V3h14v7.5a1.5 1.5 0 0 1-1.5 1.5h-5Zm7-10a.5.5 0 0 0 0-1H.5a.5.5 0 0 0 0 1h15Z" />
                     </svg>
                     <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900" name="title" value={data.title} onChange={(event) => handleInputChange(event, index)}>Title</label>
@@ -287,9 +294,9 @@ const IndexPage = () => {
                 </div>
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-vector-pen" viewBox="0 0 16 16" className='mr-[2px]'>
-                      <path fill-rule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z" />
-                      <path fill-rule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-vector-pen mr-[2px]" viewBox="0 0 16 16">
+                      <path fillRule="evenodd" d="M10.646.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-1.902 1.902-.829 3.313a1.5 1.5 0 0 1-1.024 1.073L1.254 14.746 4.358 4.4A1.5 1.5 0 0 1 5.43 3.377l3.313-.828L10.646.646zm-1.8 2.908-3.173.793a.5.5 0 0 0-.358.342l-2.57 8.565 8.567-2.57a.5.5 0 0 0 .34-.357l.794-3.174-3.6-3.6z" />
+                      <path fillRule="evenodd" d="M2.832 13.228 8 9a1 1 0 1 0-1-1l-4.228 5.168-.026.086.086-.026z" />
                     </svg>
                     <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">Author(s)</label>
                     <svg
@@ -308,7 +315,7 @@ const IndexPage = () => {
                 </div>
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-check-fill" viewBox="0 0 16 16" className='mr-[2px]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar-check-fill mr-[2px]" viewBox="0 0 16 16">
                       <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-5.146-5.146-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L7.5 10.793l2.646-2.647a.5.5 0 0 1 .708.708z" />
                     </svg>
                     <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">Date Published</label>
@@ -328,7 +335,7 @@ const IndexPage = () => {
                 </div>
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16" className='mr-[2px]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-download mr-[2px]" viewBox="0 0 16 16">
                       <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
                       <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
                     </svg>
@@ -348,7 +355,7 @@ const IndexPage = () => {
                 </div>
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-arrow-up-fill" viewBox="0 0 16 16" className='mr-[2px]'>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-file-earmark-arrow-up-fill mr-[2px]" viewBox="0 0 16 16">
                       <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM6.354 9.854a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 8.707V12.5a.5.5 0 0 1-1 0V8.707L6.354 9.854z" />
                     </svg>
                     <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">File Upload</label>
@@ -413,6 +420,36 @@ const IndexPage = () => {
                   </div>
                 ))}
 
+                <div className="mr-[50px] ml-[30px] mt-[15px]">
+                  <div className='flex'>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6z" />
+                    </svg>
+
+                    <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">Tag</label>
+                    <svg
+                      className='ml-[2px]'
+                      width="12px"
+                      height="12px"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zM262.655 90c-54.497 0-89.255 22.957-116.549 63.758-3.536 5.286-2.353 12.415 2.715 16.258l34.699 26.31c5.205 3.947 12.621 3.008 16.665-2.122 17.864-22.658 30.113-35.797 57.303-35.797 20.429 0 45.698 13.148 45.698 32.958 0 14.976-12.363 22.667-32.534 33.976C247.128 238.528 216 254.941 216 296v4c0 6.627 5.373 12 12 12h56c6.627 0 12-5.373 12-12v-1.333c0-28.462 83.186-29.647 83.186-106.667 0-58.002-60.165-102-116.531-102zM256 338c-25.365 0-46 20.635-46 46 0 25.364 20.635 46 46 46s46-20.636 46-46c0-25.365-20.635-46-46-46z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <input type="text" name="tags" onChange={handleTag} className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                    <ul className="flex flex-wrap">
+                      {tags.map((tag) => (
+                        <li key={tag} className="mt-[10px] bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                          #{tag}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+
                 <div className="mr-[50px] ml-[30px]">
                   <div className='flex mt-2 '>
                     <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900">* Note</label>
@@ -434,12 +471,13 @@ const IndexPage = () => {
             </div>
           ))}
 
-          <div class="flex justify-end mr-[50px] ml-[30px]">
-            <button class="bg-[#071d49] text-white py-2 px-4 rounded-lg">Submit</button>
+          <div className="flex justify-end mr-[50px] ml-[30px]">
+            <button className="bg-[#071d49] text-white py-2 px-4 rounded-lg">Submit</button>
           </div>
 
         </div>
       </div>
+
 
     </main>
   )
